4a20cef5aec4d15288eecd9bc202a4b213e5cf66
Alexandra Barmin <peregudova2008@gmail.com>
2023-01-16 16:38:04 +0300

    feat: add simple slider into projects

---
+++ js/slider.js
@@ -0,0 +1,9 @@
+const slider = document.querySelector(".projects__list");
+const sliderItem = slider.children;
+
+function next() { 
+	slider.append(sliderItem[0]);
+};
+function prev() { 
+	slider.prepend(sliderItem[sliderItem.length-1]);
+}
