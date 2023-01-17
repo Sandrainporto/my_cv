563913c52d82c94836bac92ff78b3ac38c1803bb
Alexandra Barmin <peregudova2008@gmail.com>
2023-01-17 15:27:42 +0300

    refactor: change courses table (order, formatting)

--- js/slider.js
+++ js/slider.js
@@ -1,8 +1,11 @@
 const slider = document.querySelector(".projects__list");
 const sliderItem = slider.children;
 
+
+
 function next() { 
 	slider.append(sliderItem[0]);
+
 };
 function prev() { 
 	slider.prepend(sliderItem[sliderItem.length-1]);
