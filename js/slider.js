59213d047576949d51cb9d59976c98d04907b643
Alexandra Barmin <peregudova2008@gmail.com>
2023-01-21 01:11:19 +0300

    feat: add burger at 940px

--- js/slider.js
+++ js/slider.js
@@ -1,3 +1,29 @@
+//бургер
+const burgerIcon = document.querySelector(".burger__icon");
+const burgerMenu = document.querySelector(".header__menu");
+const span = document.querySelector(".span");
+const opacity = document.querySelector(".opacity");
+
+burgerIcon.addEventListener("click", () => {
+	burgerIcon.classList.toggle('active');
+	burgerMenu.classList.toggle('active');
+	span.classList.toggle('active');
+	opacity.classList.toggle('active');
+})
+
+document.addEventListener('click', event => {
+if(event.target.className !== 'burger__icon active' && 
+event.target.className !== 'span active' && 
+event.target.className !== 'header__menu active') {
+	burgerIcon.classList.remove('active');
+	burgerMenu.classList.remove('active');
+	span.classList.remove('active');
+	opacity.classList.remove('active');
+}
+})
+
+
+
 const slider = document.querySelector(".projects__list");
 const sliderItem = slider.children;
 
